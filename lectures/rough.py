2115d71a4b4666ad08612351ab20a8d7c95f32a0
udaynarwal72 <72241077+udaynarwal72@users.noreply.github.com>
2020-10-29 09:49:06 +0530

    new update
    this is my new update for this repository

--- lectures/rough.py
+++ lectures/rough.py
@@ -1,10 +1,17 @@
-
-while(True):
-    a = int(input("Enter a no. greater than 100: "))
-
-    if a < 45:
-        print("Please enter a write no.")
-        continue
+n=18
+number_of_guesses=1
+print("Number of guesses is limited to only 9 times: ")
+while (number_of_guesses<=9):
+    guess_number = int(input("Guess the number :\n"))
+    if guess_number<18:
+        print("you enter less number please input greater number.\n")
+    elif guess_number>18:
+        print("you enter greater number please input smaller number.\n ")
     else:
-        print("you enter a write no.")
+        print("you won\n")
+        print(number_of_guesses,"no.of guesses he took to finish.")
         break
+    print(9-number_of_guesses,"no. of guesses left")
+    number_of_guesses = number_of_guesses + 1
+if(number_of_guesses>9):
+    print("Game Over")
--- practice/practice6.py
+++ practice/practice6.py
@@ -7,14 +7,13 @@ n = 18
 atmp = 18  # atmp= atempt
 gn = 10   # gn=guess no.
 i = 1
-print("Welcome to the game designed by Uday")
+print("Welcome to the game : designed by Uday")
 while (True):
     id1 = input("Enter your id\n")
     password = input("Enter your password\n")
     if id1 == 'Uday' and password == '123':
         while(i <= atmp):
             en = int(input("Enter the guess"))
-
             if en > gn:
                 print("Guess number:",i, "=>You guesssed high")
                 i = i+1
