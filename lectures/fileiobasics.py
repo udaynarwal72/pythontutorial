53cb01bb68d2c5cdb863eadc5f3803b7aaf2b79f
udaynarwal72 <72241077+udaynarwal72@users.noreply.github.com>
2020-10-31 22:19:19 +0530

    new

--- lectures/fileiobasics.py
+++ lectures/fileiobasics.py
@@ -1,10 +1,10 @@
 # File IO basics
 """
-"r" - Open file for reading
+"r" - Open file for reading - Default
 "w" - Open file for writing
 "x" - Creates file if not exits
 "a" - Add more content to a file
-"t" - text mode
+"t" - text mode - Default
 "b" - binary mode
 "+" - read and write
 """
