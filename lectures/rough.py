bfb9969a69d22cd6fce3860e7555d89c22c0f4df
udaynarwal72 <72241077+udaynarwal72@users.noreply.github.com>
2020-10-31 21:09:50 +0530

    new

--- lectures/rough.py
+++ lectures/rough.py
@@ -1,17 +1,8 @@
-n=18
-number_of_guesses=1
-print("Number of guesses is limited to only 9 times: ")
-while (number_of_guesses<=9):
-    guess_number = int(input("Guess the number :\n"))
-    if guess_number<18:
-        print("you enter less number please input greater number.\n")
-    elif guess_number>18:
-        print("you enter greater number please input smaller number.\n ")
-    else:
-        print("you won\n")
-        print(number_of_guesses,"no.of guesses he took to finish.")
-        break
-    print(9-number_of_guesses,"no. of guesses left")
-    number_of_guesses = number_of_guesses + 1
-if(number_of_guesses>9):
-    print("Game Over")
+def function(a,b):
+    """This is my python function that will help to find average"""
+    average=(a+b)/2
+    return average
+
+print(int(function(4,4)))
+print(function.__doc__)
+
