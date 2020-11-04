1011d917a4023ce33177a8a09153cec587438b59
udaynarwal72 <72241077+udaynarwal72@users.noreply.github.com>
2020-11-04 16:43:43 +0530

    new

--- practice/practice9special.py
+++ practice/practice9special.py
@@ -1,77 +1,57 @@
+# Game developer: Uday
 import random
-print("  \t\t\t Welecome to Snake , Water , Gun game\n\n")
+print("Welcome the snake, water and gun: by Uday ")
+game_list = ["s", "w", "g"]
+i = 1
+
+score_computer = 0
+score_you = 0
 
 
-attempts = 1
-your_point = 0
-computer_point = 0
-while (attempts <= 10):
+def winner(a):
+    """
+    This is for declaration of winner   
+    """
+    win_declare = a + " win"
+    return win_declare
 
-    lst = ["snake", "water", "gun"]
-    ran = random.choice(lst)
-    print("what do you choose snake, water or gun")
-    inp = input()
 
-    if inp == ran:
-        print("tie")
-        print(f"you choosed {inp} and computer guess is {ran}")
-        print("No body gets point\n")
+while (i <= 10):
+    print("""Enter s for Snake
+    Enter w for Water
+    Enter g for Gun""")
 
-    elif inp == "snake" and ran == "water":
-        your_point = your_point+1
-        print(f"you choosed {inp} and computer guess is {ran}")
-        print("The snake drank water\n")
-        print("You won this round")
-        print("you get 1 point\n")
-
-    elif inp == "water" and ran == "snake":
-        computer_point = computer_point + 1
-        print(f"you choosed {inp} and computer guess is {ran}")
-        print("The snake drank water\n")
-        print("You Lost this round")
-        print("computer gets 1 point\n")
-
-    elif inp == "water" and ran == "gun":
-        print(f"you choosed {inp} and computer guess is {ran}")
-        your_point = your_point + 1
-        print("The gun sank in water\n")
-        print("You won this round")
-        print("you get 1 point\n")
-
-    elif inp == "gun" and ran == "water":
-        print(f"you choosed {inp} and computer guess is {ran}")
-        computer_point = computer_point + 1
-        print("The gun sank in water\n")
-        print("You Lost this round")
-        print("computer gets 1 point\n")
-
-    elif inp == "gun" and ran == "snake":
-        print(f"you choosed {inp} and computer guess is {ran}")
-        your_point = your_point + 1
-        print("The snake was shoot and he died\n")
-        print("You Won this round")
-        print("you get 1 point\n")
-
-    elif inp == "snake" and ran == "gun":
-        print(f"you choosed {inp} and computer guess is {ran}")
-        computer_point = computer_point+1
-        print("The snake was shoot and he died\n")
-        print("You Lost this round")
-        print("computer gets 1 point\n")
-
+    user_choice = input("Enter your choice: ")
+    choice_game_list = random.choice(game_list)
+    if choice_game_list == user_choice:
+        print(winner("no one"))
+    elif choice_game_list == "s" and user_choice == "w":
+        score_computer = score_computer + 1
+        print(winner("Computer"))
+    elif choice_game_list == "s" and user_choice == "g":
+        score_you = score_you + 1
+        print(winner("You"))
+    elif choice_game_list == "w" and user_choice == "s":
+        score_you = score_you + 1
+        print(winner("You"))
+    elif choice_game_list == "w" and user_choice == "g":
+        score_computer = score_computer + 1
+        print(winner("Computer"))
+    elif choice_game_list == "g" and user_choice == "s":
+        score_computer = score_computer + 1
+        print(winner("Computer"))
+    elif choice_game_list == "g" and user_choice == "w":
+        score_you = score_you + 1
+        print(winner("You"))
     else:
-        print("invalid")
-
-    print(10 - attempts, "no. of guesses left")
-    attempts = attempts + 1
-
-    if attempts > 10:
-        print("game over")
-
-if computer_point > your_point:
-    print("Computer wins and you loose")
-
-if computer_point < your_point:
-    print("you win and computer loose")
-
-print(f"your point is {your_point} and computer point is {computer_point}")
+        print("please Enter a write input")
+    print("Your score is ", score_you)
+    print("Computer score is ", score_computer)
+    if score_you > score_computer:
+        print("You win")
+    elif score_you < score_computer:
+        print("Computer win")
+        print("Better luck next time")
+    elif score_you == score_computer:
+        print("tie between you and comp")
+    i += 1
